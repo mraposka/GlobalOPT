b42f93391aa6d5d938fa1b8273d62d6e4804862d
Abdulkadir Can Kinsiz <kadircankinsiz@gmail.com>
2022-09-21 15:14:15 +0300

    Basic Design and Code Importing

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,13 +28,226 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown3 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown4 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown5 = new System.Windows.Forms.NumericUpDown();
+            this.output = new System.Windows.Forms.RichTextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown5)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(299, 31);
+            this.button2.Margin = new System.Windows.Forms.Padding(10, 9, 10, 9);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(54, 45);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Restart Docker";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click_1);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(45, 521);
+            this.button3.Margin = new System.Windows.Forms.Padding(10, 9, 10, 9);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(580, 65);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Run";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(161, 72);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(10, 9, 10, 9);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(31, 44);
+            this.textBox1.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(38, 40);
+            this.label1.Margin = new System.Windows.Forms.Padding(10, 0, 10, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(154, 37);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Algorithm";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(373, 32);
+            this.comboBox1.Margin = new System.Windows.Forms.Padding(10, 9, 10, 9);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(252, 45);
+            this.comboBox1.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(38, 125);
+            this.label2.Margin = new System.Windows.Forms.Padding(10, 0, 10, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(132, 37);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Iteration";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(38, 296);
+            this.label3.Margin = new System.Windows.Forms.Padding(10, 0, 10, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(102, 37);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Depth";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(38, 467);
+            this.label4.Margin = new System.Windows.Forms.Padding(10, 0, 10, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(66, 37);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "BFI";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(38, 211);
+            this.label5.Margin = new System.Windows.Forms.Padding(10, 0, 10, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(160, 37);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Threshold";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(38, 381);
+            this.label6.Margin = new System.Windows.Forms.Padding(10, 0, 10, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(297, 37);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "Number of Matrices";
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(373, 379);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(252, 44);
+            this.numericUpDown1.TabIndex = 11;
+            // 
+            // numericUpDown2
+            // 
+            this.numericUpDown2.Location = new System.Drawing.Point(373, 125);
+            this.numericUpDown2.Name = "numericUpDown2";
+            this.numericUpDown2.Size = new System.Drawing.Size(252, 44);
+            this.numericUpDown2.TabIndex = 12;
+            // 
+            // numericUpDown3
+            // 
+            this.numericUpDown3.Location = new System.Drawing.Point(373, 209);
+            this.numericUpDown3.Name = "numericUpDown3";
+            this.numericUpDown3.Size = new System.Drawing.Size(252, 44);
+            this.numericUpDown3.TabIndex = 13;
+            // 
+            // numericUpDown4
+            // 
+            this.numericUpDown4.Location = new System.Drawing.Point(373, 294);
+            this.numericUpDown4.Name = "numericUpDown4";
+            this.numericUpDown4.Size = new System.Drawing.Size(252, 44);
+            this.numericUpDown4.TabIndex = 14;
+            // 
+            // numericUpDown5
+            // 
+            this.numericUpDown5.Location = new System.Drawing.Point(373, 465);
+            this.numericUpDown5.Name = "numericUpDown5";
+            this.numericUpDown5.Size = new System.Drawing.Size(252, 44);
+            this.numericUpDown5.TabIndex = 15;
+            // 
+            // output
+            // 
+            this.output.BackColor = System.Drawing.Color.Black;
+            this.output.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.output.ForeColor = System.Drawing.Color.White;
+            this.output.Location = new System.Drawing.Point(638, 31);
+            this.output.Name = "output";
+            this.output.Size = new System.Drawing.Size(802, 555);
+            this.output.TabIndex = 16;
+            this.output.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(19F, 37F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1452, 615);
+            this.Controls.Add(this.output);
+            this.Controls.Add(this.numericUpDown5);
+            this.Controls.Add(this.numericUpDown4);
+            this.Controls.Add(this.numericUpDown3);
+            this.Controls.Add(this.numericUpDown2);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(10, 9, 10, 9);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown5)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.NumericUpDown numericUpDown2;
+        private System.Windows.Forms.NumericUpDown numericUpDown3;
+        private System.Windows.Forms.NumericUpDown numericUpDown4;
+        private System.Windows.Forms.NumericUpDown numericUpDown5;
+        private System.Windows.Forms.RichTextBox output;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -2,11 +2,18 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
+using System.IO;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+using System.Xml.Linq;
+using System.Reflection;
 
 namespace GlobalOPT
 {
@@ -15,6 +22,257 @@ namespace GlobalOPT
         public Form1()
         {
             InitializeComponent();
+        }
+
+        [DllImport("user32.dll")]
+        private static extern bool ShowWindow(IntPtr hwnd, int nCmdShow);
+
+        static bool softwareInstallation = false;
+        static string strCmdText;
+        static string tag = "fw";
+        static string name = "opt";
+        static string gitRepoURL = "https://github.com/mraposka/DockerOPT";
+        static string gitRepoName = gitRepoURL.Split('/').Last();
+        static string desktopPath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+        static string winDir = Path.GetPathRoot(System.Environment.GetEnvironmentVariable("WINDIR"));
+        static string gitRepoClonePath = desktopPath + "\\GlobalOPTFW\\" + gitRepoName + "\\globalopt";
+        static string dockerFilePath = desktopPath + "\\GlobalOPTFW\\" + gitRepoName;
+        static string textFilePath = desktopPath + "\\result.txt";
+        static string algorithm = "", iteration = "", threshold = "", depth = "", num_matrices = "", bfi = "";
+
+        void RunDockerBuild()//run the docker image
+        {
+            //Runs created docker build
+            Echo("Executing...");
+            Thread.Sleep(100);
+            strCmdText = "/C @echo off &  cd " + gitRepoClonePath + " & docker run " + tag + ":" + name;
+            string output = RunCommand();
+            Echo("Execution completed!");
+            //Runs created docker build
+            //Saving results in result.txt file 
+            Echo("Saving results...");
+            string buildOutput = "------Build Time:" + DateTime.Now + "------\n" + output + "------Build Finished------\n";
+            if (!File.Exists(textFilePath))
+                File.Create(textFilePath).Close();//Create result.txt file if does not exist
+            File.AppendAllText(textFilePath, buildOutput);
+            Echo("Result file succesfully written on result file on Desktop\n\nPress any key to exit");
+            //Saving results in result.txt file
+        }
+        void InstallRequiredSoftwares()//Install docker and git if its not installed
+        {
+            GitInstall();
+            DockerInstall();
+        }
+        void GitInstall()//Installing Git via powershell
+        {
+            Echo("Checking Git...");
+            strCmdText = "/C git -v";
+            string output = RunCommand();//If git is installed output will be version of git. So it cant be null
+            if (string.IsNullOrEmpty(output))
+            {
+                //Installing Git
+                Echo("Git not found. Installing Git...");
+                string installCommand = "$git_url = \"https://api.github.com/repos/git-for-windows/git/releases/latest\"\r\n$asset = Invoke-RestMethod -Method Get -Uri $git_url | % assets | where name -like \"*64-bit.exe\"\r\n# download installer\r\n$installer = \"$env:temp\\$($asset.name)\"\r\nInvoke-WebRequest -Uri $asset.browser_download_url -OutFile $installer\r\n# run installer\r\n$git_install_inf = \"<install inf file>\"\r\n$install_args = \"/SP- /VERYSILENT /SUPPRESSMSGBOXES /NOCANCEL /NORESTART /CLOSEAPPLICATIONS /RESTARTAPPLICATIONS /LOADINF=\"\"$git_install_inf\"\"\"\r\nStart-Process -FilePath $installer -ArgumentList $install_args -Wait";
+                File.Create("gitinstaller.txt").Close();
+                File.WriteAllText("gitinstaller.txt", installCommand);
+                File.Move("gitinstaller.txt", Path.ChangeExtension("gitinstaller.txt", ".ps1"));
+                var startInfo = new ProcessStartInfo()
+                {
+                    FileName = "powershell.exe",
+                    Arguments = $"-NoProfile -ExecutionPolicy ByPass -File \"gitinstaller.ps1\"",
+                    Verb = "runas",
+                    UseShellExecute = false
+                };
+                Process process = new Process();
+                process.StartInfo = startInfo;
+                process.Start();
+                process.WaitForExit();
+                Echo("Git installed");
+                softwareInstallation = true;//is any software installed
+                Restart();//Restarting program for git commands
+                //Installing Git 
+            }
+            else
+                Echo("Git found.");
+        }
+        void Restart()//Restart the application
+        {
+            System.Diagnostics.Process.Start(Assembly.GetExecutingAssembly().Location);
+            Environment.Exit(0);
+        }
+        void DockerInstall()//Installing Docker via powershell
+        {
+            Echo("Checking Docker...");
+            strCmdText = "/C docker -v";
+            string output = RunCommand();//If docker is installed output will be version of git. So it cant be null
+            if (string.IsNullOrEmpty(output))
+            {
+                //Installing Choco And Docker
+                Echo("Docker not found. Installing Docker...");
+                //choco uninstalling (for reinstall *temporary solution*) 
+                try { Directory.Delete(System.Environment.GetFolderPath(System.Environment.SpecialFolder.CommonApplicationData) + "\\chocolatey"); } catch { }
+
+                //choco uninstalling (for reinstall *temporary solution*)
+                string installCommand = "Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString(\"https://chocolatey.org/install.ps1\"))\r\nchoco install docker-desktop -y";
+                File.Create("dockerinstaller.txt").Close();
+                File.WriteAllText("dockerinstaller.txt", installCommand);
+                File.Move("dockerinstaller.txt", Path.ChangeExtension("dockerinstaller.txt", ".ps1"));
+                var startInfo = new ProcessStartInfo()
+                {
+                    FileName = "powershell.exe",
+                    Arguments = $"-NoProfile -ExecutionPolicy ByPass -File \"dockerinstaller.ps1\"",
+                    Verb = "runas",
+                    UseShellExecute = false
+                };
+                Process process = new Process();
+                process.StartInfo = startInfo;
+                process.Start();
+                process.WaitForExit();
+                Echo("Docker installed");
+                softwareInstallation = true;//is any software installed
+                //Installing Choco And Docker 
+            }
+            else
+                Echo("Docker found.");
+
+        }
+        void StartDocker()//Starts docker.backend
+        {
+            bool procK = false;
+            var proc = Process.Start(Path.Combine(winDir + @"Program Files\Docker\Docker\resources\com.docker.backend.exe"));//Docker backend path (default)
+            while (!procK)
+            {
+                foreach (var process in Process.GetProcessesByName("Docker Desktop"))
+                { process.Kill(); procK = true; }
+
+            }
+            while (proc.MainWindowHandle == IntPtr.Zero) //note: only works as long as your process actually creates a main window.
+                System.Threading.Thread.Sleep(10);
+            ShowWindow(proc.MainWindowHandle, 0);//Hide command window for better visualization
+        }
+
+        void CreateDockerImage()//Building docker image with params
+        {
+            //Building docker image
+            Echo("Getting parameters...");
+            string oldParameters = File.ReadAllText(gitRepoClonePath + "\\parameter.txt");
+            string[] lines = oldParameters.Split(new string[] { Environment.NewLine }, StringSplitOptions.None);
+            if (algorithm != "0")//Implementing parameters if added
+            {
+                Echo("Implementing parameters...");
+                algorithm = algorithm != "0" ? algorithm : lines[0].Split('=')[1];
+                iteration = iteration != string.Empty ? iteration : lines[2].Split('=')[1];
+                threshold = threshold != string.Empty ? threshold : lines[3].Split('=')[1];
+                depth = depth != string.Empty ? depth : lines[4].Split('=')[1];
+                num_matrices = num_matrices != string.Empty ? num_matrices : lines[5].Split('=')[1];
+                bfi = bfi != string.Empty ? bfi : lines[6].Split('=')[1];
+                ChangeParameters();//Change parameter.txt with inserted params
+                Echo("Implementing completed");
+            }
+            Thread.Sleep(100);
+            Echo("Building Docker Image...");
+            strCmdText = "/C @echo off &  cd " + dockerFilePath + " & docker build . -t " + tag + ":" + name;
+            RunCommand();
+            Echo("Docker Image Built Succesfully!");
+            //Building docker image  
+        }
+        void GitClone()//Cloning git repo on Desktop 
+        {
+            //Cloning git repo on Desktop 
+            Echo("Cloning Git Repository...");
+            strCmdText = "/C @echo off &  cd " + desktopPath + " & mkdir GlobalOPTFW & cd GlobalOPTFW & git clone " + gitRepoURL;
+            string output = RunCommand();
+            Echo(output + "Git Repo Cloned!");
+            //Cloning git repo on Desktop
+        }
+
+        void ClearAndUpdate()//Clear old builds and update repo if need
+        {
+            //Updating repo
+            Echo("Clearing old builds and updating repository...");
+            if (Directory.Exists(gitRepoClonePath))//if repo exists update repo
+            {
+                strCmdText = "/C @echo off &  cd " + gitRepoClonePath + " & git pull";
+                RunCommand();
+                Echo("Git Repo Updated To Latest Version!");
+            }
+            else
+            {
+                Echo("Git Repo Not Found!", Color.Red);
+            }
+            //Updating repo 
+            //Clearing all docker images 
+            Echo("Clearing old docker images...");
+            strCmdText = "/C for /F %i in ('docker images -a -q') do docker rmi -f %i";
+            RunCommand();
+            Echo("All Docker Image Cleared!");
+            //Clearing all docker images
+            Echo("All Cleared");
+        }
+
+        void ChangeParameters()
+        {
+            //Changing the parameter.txt
+            string parameter = "ALGORITHM=" + algorithm + "\r\nMATRIX_PATH=matrix.txt\r\nITERATION=" + iteration + "\r\nTHRESHOLD=" + threshold + "\r\nDEPTH=" + depth + "\r\nNUM_MATRICES=" + num_matrices + "\r\nBFI=" + bfi + "";
+            File.WriteAllText(gitRepoClonePath + "\\parameter.txt", parameter);
+            //Changing the parameter.txt
+        }
+        string RunCommand()//Function that run commands on cmd.exe
+        {
+            Process process = new System.Diagnostics.Process();
+            ProcessStartInfo startInfo = new System.Diagnostics.ProcessStartInfo();
+            startInfo.WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden;
+            startInfo.FileName = "cmd.exe";
+            startInfo.UseShellExecute = false;
+            startInfo.RedirectStandardOutput = true;
+            startInfo.Arguments = strCmdText;
+            process.StartInfo = startInfo;
+            process.Start();
+            string output = process.StandardOutput.ReadToEnd();
+            process.WaitForExit();
+            Console.WriteLine(output);
+            Thread.Sleep(100);
+            return output;
+        }
+
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+            KillDocker();
+            Thread.Sleep(100);
+            StartDocker();
+        }
+
+        static void ProcKil(string proc)
+        {
+            foreach (var process in Process.GetProcessesByName(proc))
+                process.Kill();
+        }
+        static void KillDocker()
+        {
+            ProcKil("docker");
+            ProcKil("com.docker.vpnkit");
+            ProcKil("com.docker.proxy");
+            ProcKil("com.docker.backend");
+            ProcKil("Docker Desktop");
+        } 
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Echo("Welcome to the GlobalOPT");
+            Thread.Sleep(1000);
+            Echo("Checking required softwares...", Color.Red);
+        }
+
+        void Echo(string text, Color color = default)//function that logs with color on terminal 
+        {
+            output.SelectionStart = output.TextLength;
+            output.SelectionLength = 0;
+
+            output.SelectionColor = color == default ? Color.LimeGreen : color;
+            output.AppendText(text + Environment.NewLine);
+            output.SelectionColor = output.ForeColor;
+
+            Thread.Sleep(100);
         }
     }
 }
